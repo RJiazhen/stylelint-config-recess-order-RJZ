77943546b3df86930c358a3d0f58ac6c4dacf592
RJiazhen <jiazhen.ruan@outlook.com>
2023-07-08 12:53:46 +0800

    feat: 设置各组的默认参数

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const propertyGroups = require('./groups.js')
 module.exports = {
 	plugins: ['stylelint-order'],
 	rules: {
-		'order/properties-order': propertyGroups,
+		'order/properties-order': propertyGroups.map((group) => ({
+			// 默认同组属性之前需要空行
+			emptyLineBefore: group?.emptyLineBefore || 'always',
+			// 默认同组属性之间不允许空行
+			noEmptyLineBetween: true,
+			properties: group.properties,
+		})),
 	},
 }
